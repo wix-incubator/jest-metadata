967d206347ffb87c5ca802d051c7f0f683131fc4
Yaroslav Serhieiev <noomorph@gmail.com>
2023-08-04 17:25:21 +0300

    ci: fix 2

--- scripts/setup-fixture.mjs
+++ scripts/setup-fixture.mjs
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-import 'zx/globals';
+import fs from 'node:fs';
+import path from 'node:path';
+import process from 'node:process';
 
 const recorderRootDir = path.join(process.cwd(), 'packages/recorder')
 const recorderManifestPath = path.join(recorderRootDir, 'package.json');
@@ -9,4 +11,4 @@ const jestVersion = process.env.JEST_VERSION || 'latest';
 recorderManifest.devDependencies.jest = jestVersion;
 fs.writeFileSync(recorderManifestPath, JSON.stringify(recorderManifest, null, 2) + '\n');
 
-console.log(`Setting jest@${chalk.red(jestVersion)}\n`);
+console.log(`Setting jest@${jestVersion}\n`);
