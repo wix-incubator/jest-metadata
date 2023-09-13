e0f762a1e79737657706db7797e2f706202e2450
Yaroslav Serhieiev <noomorph@gmail.com>
2023-09-13 11:47:54 +0300

    wip: strict external facade

--- packages/library/src/metadata/dsl/MetadataDSL.ts
+++ packages/library/src/metadata/dsl/MetadataDSL.ts
@@ -5,6 +5,7 @@ export class MetadataDSL {
   readonly #metadata: () => Metadata;
   readonly #emitter: ReadonlyMetadataEventEmitter;
   readonly #scheduled = new Set<() => void>();
+  #initialized = false;
   #running = false;
 
   constructor(emitter: ReadonlyMetadataEventEmitter, metadata: () => Metadata) {
@@ -23,6 +24,9 @@ export class MetadataDSL {
         this.#scheduled.clear();
       })
       .on('add_test_file', () => {
+        // Assert that the metadata is not disconnected from the emitter.
+        // TODO: move to 'setup' Circus event
+        this.#initialized = true;
         // When running `jest --runInBand`, the MetadataDSL instance is shared between
         // multiple test files. We need to reset the running mode, when a new test file
         // is added.
@@ -31,12 +35,17 @@ export class MetadataDSL {
   }
 
   $Set = (path: string | readonly string[], value: unknown): void => {
+    this.#assertPath(path);
+
     this.schedule(() => {
       this.#metadata().set(path, value);
     });
   };
 
   $Push = (path: string | readonly string[], ...values: unknown[]): void => {
+    this.#assertPath(path);
+    this.#assertValues(values);
+
     this.schedule(() => {
       const metadata = this.#metadata();
       metadata.push(path, values);
@@ -44,21 +53,26 @@ export class MetadataDSL {
   };
 
   $Assign = (path: string | readonly string[] | undefined, value: Data): void => {
+    this.#assertPath(path);
+    this.#assertValue(value);
+
     this.schedule(() => {
       this.#metadata().assign(path, value);
     });
   };
 
   $Merge = (path: string | readonly string[] | undefined, value: Data): void => {
+    this.#assertPath(path);
+    this.#assertValue(value);
+
     this.schedule(() => {
       this.#metadata().merge(path, value);
     });
   };
 
   protected schedule(callback: () => void): void {
-    if (this.#running) {
-      throw new JestMetadataError('Cannot apply annotations when tests are already running.');
-    }
+    this.#assertInitialized();
+    this.#assertNotRunning();
 
     const callbackAutoDelete = () => {
       this.#scheduled.delete(callbackAutoDelete);
@@ -67,5 +81,35 @@ export class MetadataDSL {
 
     this.#scheduled.add(callbackAutoDelete);
     this.#emitter.once('*', callbackAutoDelete);
+  }
+
+  #assertInitialized(): void {
+    if (!this.#initialized) {
+      console.warn('TODO: come up with a better error message');
+    }
+  }
+
+  #assertNotRunning(): void {
+    if (this.#running) {
+      throw new JestMetadataError('Cannot apply annotations when tests are already running.');
+    }
+  }
+
+  #assertPath(path: string | readonly string[] | undefined): void {
+    if (path === undefined) {
+      throw new JestMetadataError('Cannot apply annotations without a path.');
+    }
+  }
+
+  #assertValue(value: Data): void {
+    if (value === undefined) {
+      throw new JestMetadataError('Cannot apply annotations without a defined value.');
+    }
+  }
+
+  #assertValues(values: unknown[]): void {
+    if (values.length === 0) {
+      throw new JestMetadataError('Cannot apply annotations without values.');
+    }
   }
 }
