8e7d2b99c27fda7c6a69ab779b544cb358ba75fe
Yaroslav Serhieiev <noomorph@gmail.com>
2023-09-15 09:42:39 +0300

    amend 3

--- packages/library/src/environment-node.ts
+++ packages/library/src/environment-node.ts
@@ -1,5 +1,6 @@
 import JestEnvironmentNode from 'jest-environment-node';
 import { WithMetadata } from './environment-decorator';
 
+export { ForwardedCircusEvent } from './environment-hooks';
 export const TestEnvironment = WithMetadata(JestEnvironmentNode);
 export default TestEnvironment;
