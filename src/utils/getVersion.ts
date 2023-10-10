a63f156cbc9e7f66c31f7e677d49b70d03eb8b4c
Yaroslav Serhieiev <noomorph@gmail.com>
2023-10-10 16:13:37 +0300

    perf: reduce footprint

--- src/utils/getVersion.ts
+++ src/utils/getVersion.ts
@@ -1,9 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
 
-import memoize from 'lodash.memoize';
+import { memoizeLast } from './memoizeLast';
 
-export const getVersion = memoize(() => {
+export const getVersion = memoizeLast(() => {
   const packageJsonPath = path.join(__dirname, '../../package.json');
   const packageJsonRaw = fs.readFileSync(packageJsonPath, 'utf8');
   const packageJson = JSON.parse(packageJsonRaw);
