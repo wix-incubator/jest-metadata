b96e13d4a3cc275b71d5627a34b26c3b62d1c5f8
Yaroslav Serhieiev <noomorph@gmail.com>
2023-10-14 13:36:24 +0300

    fix: avoid mutations in queued IPC messages

--- src/ipc/BatchMessage.ts
+++ src/ipc/BatchMessage.ts
@@ -1,8 +1,6 @@
-import type { MetadataEvent } from '../metadata';
-
 export type BatchMessage = {
-  /** The batch of messages. */
-  batch: MetadataEvent[];
+  /** The batch of stringified messages. */
+  batch: string[];
   /** Whether this is the first batch of messages. */
   first?: boolean;
   /** Whether this is the last batch of messages. */
