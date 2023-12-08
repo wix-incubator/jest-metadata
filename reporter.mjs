ac1346dd7a8c21fde555d2d904e8818e5501a837
Yaroslav Serhieiev <yaroslavs@wix.com>
2023-12-08 22:03:58 +0200

    fix: improve CJS and ESM exports compatibility with older version (#69)

--- reporter.mjs
+++ reporter.mjs
@@ -2,4 +2,6 @@ import reporter from './dist/reporter.js';
 
 const { default: JestMetadataReporter } = reporter;
 
+const query = JestMetadataReporter.query;
+export { query, JestMetadataReporter };
 export default JestMetadataReporter;
